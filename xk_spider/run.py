ba74f0ede26e1141b49004312f2bbf185fbc4a7a
Haoyue Shi <shihaoyue36@gmail.com>
2022-01-30 11:01:59 +0800

    add 验证码手动输入

--- xk_spider/run.py
+++ xk_spider/run.py
@@ -1,8 +1,10 @@
+import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from Login import get_params
 
 import GetCourse
+import GUI
 
 # 程序运行后会打开浏览器进入选课登录页面，请登录进去直到能看到具体的课程，然后就可以把浏览器关了
 headers = {
@@ -12,19 +14,28 @@ headers = {
 }
 url = 'http://xk.ynu.edu.cn/xsxkapp/sys/xsxkapp/*default/index.do'
 
-stdCode = ' '  # 在''中填入你的学号
-pswd = ' '  # 填你的密码,如果你有安全上的考虑也可以等浏览器打开了再填
+GUI.login_gui()
 
+f = open("info.json", "r")
+info = json.loads(f.read())
+
+stdCode = info['user']['num']
+pswd = info['user']['psw']
 
 headers['cookie'], headers['token'], batchCode = get_params(stdCode, pswd, free=True)
-headers['Referer'] = 'http://xk.ynu.edu.cn/xsxkapp/sys/xsxkapp/*default/grablessons.do?token='+headers["token"]
+headers['Referer'] = 'http://xk.ynu.edu.cn/xsxkapp/sys/xsxkapp/*default/grablessons.do?token=' + headers["token"]
 gc = GetCourse.GetCourse(headers, stdCode, batchCode)
 ec = ThreadPoolExecutor()
 
-# 仿照例子填写，可填写多个
-taskList = [ec.submit(gc.judge, '大学英语读写', '高帆', class_id='202120221YN300417000826', delete_name="大学英语读写", kind='主修')
-            # ec.submit(gc.judge, '大学英语听说', '陈俊秋', class_id='202120221YN300417000726', delete_name="大学英语听说", kind='主修')
-            ]
-# def judge(self, course_name, teacher, delete_name='', delete_teacher='', class_id='', delete_id='', kind='素选'):
+taskList = []
+for course in info["course_list"]:
+    taskList.append(ec.submit(gc.judge,
+                              course['course_name'],
+                              course['teacher'],
+                              course['delete_name'],
+                              course['delete_teacher'],
+                              course['class_id'],
+                              course['delete_id'],
+                              course['素选']))
 for future in as_completed(taskList):
     print(future.result())
